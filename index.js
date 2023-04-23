65c1941c0241d6354641972fad0f05bf5398bec5
Marisa Damron <mmarisadamronn@icloud.com>
2023-04-23 00:56:27 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(task = "go to the office") {
+    return `This Monday, I will ${task}.`;
+}
+function wrapAdjective(string="*"){
+    return function(value2="special"){
+         return `You are ${string}${value2}${string}!`
+}
+}
